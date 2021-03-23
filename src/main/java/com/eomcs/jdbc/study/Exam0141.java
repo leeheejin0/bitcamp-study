e76a3469118b2a810914bc2e8375f0bfb50505de
leeheejin <leeheejin@leeheejinui-MacBookPro.local>
2021-03-23 20:08:24 +0900

    .

--- src/main/java/com/eomcs/jdbc/study/Exam0141.java
+++ src/main/java/com/eomcs/jdbc/study/Exam0141.java
@@ -1,10 +1,19 @@
 package com.eomcs.jdbc.study;
 
+import java.sql.DriverManager;
+
 public class Exam0141 {
 
   public static void main(String[] args) {
-    // TODO Auto-generated method stub
+    System.out.printf("jdbc.drivers=%s\n", System.getProperty("jdbc.drivers"));
 
+    try {
+      java.sql.Driver driver = DriverManager.getDriver("jdbc:mariadb:");
+      System.out.println(driver);
+      System.out.println("test!");
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
 }
